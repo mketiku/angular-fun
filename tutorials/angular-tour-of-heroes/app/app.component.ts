54dc73b6d83f6adcaf5619f736ecc0ee2e155039
michael ketiku <mketiku@gmail.com>
2016-11-19 11:52:42 -0500

    init tour of heroes

---
+++ tutorials/angular-tour-of-heroes/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: `
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>`
+})
+export class AppComponent {
+    title = 'Tour of Heroes';
+    hero: Hero = {
+        id: 1,
+        name: 'Windstorm'
+    };
+}
+
+export class Hero {
+    id: number; // hero has an id and a name
+    name: string;
+}
