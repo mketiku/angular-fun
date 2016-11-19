70cd8106c69b58695c4add98280b8c47eb8f8e4d
michael ketiku <mketiku@gmail.com>
2016-11-19 11:36:25 -0500

    new index and style

--- tutorials/reddit-clone/src/app/app.component.ts
+++ tutorials/reddit-clone/src/app/app.component.ts
@@ -1,27 +1,27 @@
-import {
-  Component
-} from '@angular/core';
+import {  Component,Input } from '@angular/core';
 
-
+@Component({
+  selector: 'app-article',
+  template: `
+    <div>
+    <h2>{{ article.title }}</h2>
+      Article list will go here
+    </div>
+  `
+})
+export class ArticleComponent {
+  @Input() article: Object;// tell angular we are expecting input on the article component 
+}
 @Component({
   selector: 'app-sidebar',
   template: `
     <div id="sidebar">
         Sidebar will go here
     </div>
-  `,
+  `
 })
 export class SidebarComponent {}
 
-@Component({
-  selector: 'app-article',
-  template: `
-    <div>
-      Article list will go here
-    </div>
-  `,
-})
-export class ArticleComponent {}
 
 @Component({ // Decorator: Function that accepts a function and decorates the class app component
   selector: 'app-root', // Defines what tag to render to
@@ -29,16 +29,19 @@ export class ArticleComponent {}
     <div id="container">
       <app-sidebar></app-sidebar>
       <div id="content">
-        <app-article></app-article>
-        <app-article></app-article>
-        <app-article></app-article>
-        <app-article></app-article>
-        <app-article></app-article>
-
-      </div>
+        <app-article>
+         [article]="article"</app-article>
+       </div>
     </div>
-  `,
+  `
 })
 export class AppComponent {
-  
+  article: Object;
+  // instance variable for article component
+  constructor() {
+      this.article = {
+        title: 'The Angular 2 Book ',
+        description: 'The  best way to learn Angular 2. '
+      };
+    } // first function to run is the constructor
 }
