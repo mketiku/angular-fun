fe1cb27d57bdeb2970244e76e781b9dfd8beb650
michael ketiku <mketiku@gmail.com>
2016-11-19 11:58:32 -0500

    instantiate an hero object and use in template

--- tutorials/angular-tour-of-heroes/app/app.component.ts
+++ tutorials/angular-tour-of-heroes/app/app.component.ts
@@ -4,11 +4,15 @@ import { Component } from '@angular/core';
     selector: 'my-app',
     template: `
     <h1>{{title}}</h1>
-    <h2>{{hero.name}} details!</h2>`
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div><label>name: </label>{{hero.name}}</div>
+
+    `
 })
 export class AppComponent {
     title = 'Tour of Heroes';
-    hero: Hero = {
+    hero: Hero = { //initialize hero as atype of Hero
         id: 1,
         name: 'Windstorm'
     };
