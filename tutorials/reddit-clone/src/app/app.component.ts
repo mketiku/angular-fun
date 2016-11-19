db45e2b32645c4a67e175db05fcab69e0ad9ff7a
michael ketiku <mketiku@gmail.com>
2016-11-18 21:33:10 -0500

    init app, article and sidebar components

--- tutorials/reddit-clone/src/app/app.component.ts
+++ tutorials/reddit-clone/src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import {
+  Component
+} from '@angular/core';
+
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-sidebar',
+  template: `
+    <div id="sidebar">
+        Sidebar will go here
+    </div>
+  `,
+})
+export class SidebarComponent {}
+
+@Component({
+  selector: 'app-article',
+  template: `
+    <div>
+      Article list will go here
+    </div>
+  `,
+})
+export class ArticleComponent {}
+
+@Component({ // Decorator: Function that accepts a function and decorates the class app component
+  selector: 'app-root', // Defines what tag to render to
+  template: `
+    <div id="container">
+      <app-sidebar></app-sidebar>
+      <div id="content">
+        <app-article></app-article>
+        <app-article></app-article>
+        <app-article></app-article>
+        <app-article></app-article>
+        <app-article></app-article>
+
+      </div>
+    </div>
+  `,
 })
 export class AppComponent {
-  title = 'app works!';
+  
 }
--- tutorials/reddit-clone/src/app/app.module.ts
+++ tutorials/reddit-clone/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
+import { AppComponent,SidebarComponent, ArticleComponent } from './app.component'; // import this here 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent, //list this as a declaration
+    ArticleComponent   //list this as a declaration
   ],
   imports: [
     BrowserModule,
